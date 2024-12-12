f28292cc43b962dca512d0ab8e8c98d0db081628
shanin18 <shamimhosan02@gmail.com>
2024-12-12 20:52:04 +0600

    completed activities

--- src/components/DataCard.jsx
+++ src/components/DataCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const DataCard = ({ data }) => {
-  const { type, title, status, lastActivity } = data;
+const DataCard = ({ data, children }) => {
+  const { type, title } = data;
 
   return (
     <div className="rounded-2xl bg-[#242424] py-4 px-6 hover:shadow-[2px_2px_4px] hover:shadow-[#009696] duration-300 ease-in-out">
+      {/* card header */}
       <div className="flex items-center justify-between mb-4">
         <div className="flex py-1 px-2 justify-center items-center gap-1 rounded-lg border border-[#009696] bg-secondPrimary text-[#ccc] text-xs leading-[21px] tracking-[-0.12px]">
           <span className="w-[9px] h-[9px] rounded-full bg-[#009696]"></span>
@@ -32,19 +33,12 @@ const DataCard = ({ data }) => {
           </svg>
         </button>
       </div>
-      <div>
-        <h3 className="text-light text-xl font-semibold leading-[21px] tracking-[-0.2px] mb-6">
-          {title}
-        </h3>
-        <div className="flex items-center justify-between">
-          <p className="text-secondary text-xs font-medium leading-[21px] tracking-[-0.12px]">
-            Status: <span className="text-light">{status}</span>
-          </p>
-          <p className="text-secondary text-xs font-medium leading-[21px] tracking-[-0.12px]">
-            Last Activity: <span className="text-light">{lastActivity}</span>
-          </p>
-        </div>
-      </div>
+
+      {/* card-title */}
+      <h3 className="text-light text-xl font-semibold leading-[21px] tracking-[-0.2px]">
+        {title}
+      </h3>
+      {children}
     </div>
   );
 };
--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -1,7 +1,13 @@
+import DataCard from "@/components/DataCard";
+import Pagination from "@/components/Pagination";
 import ProgressCard from "@/components/ProgressCard";
-import React from "react";
+import CalenderSvg from "@/components/svgs/CalenderSvg";
+import ClockSvg from "@/components/svgs/ClockSvg";
+import { useGetSystemsData } from "@/hooks/useGetSystemsData";
+import { useState } from "react";
 
 const Activities = () => {
+  const [currentPage, setCurrentPage] = useState(1);
   const data = [
     {
       title: "Calls Today",
@@ -29,11 +35,99 @@ const Activities = () => {
     },
   ];
 
+  const {
+    data: allData,
+    isLoading,
+    isError,
+    error,
+  } = useGetSystemsData("open houses");
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
+
+  const completedData = allData?.filter(
+    (item) => item?.status.toLowerCase() == "completed"
+  );
+
+  const scheduledData = allData?.filter(
+    (item) => item?.status.toLowerCase() == "scheduled"
+  );
+
   return (
-    <div className="grid grid-cols-4 gap-1 py-[25px] bg-[#1E1E1E]">
-      {data?.map((item, idx) => (
-        <ProgressCard key={idx} data={item} />
-      ))}
+    <div>
+      <div className="grid grid-cols-4 gap-1 py-[25px] bg-[#1E1E1E]">
+        {data?.map((item, idx) => (
+          <ProgressCard key={idx} data={item} />
+        ))}
+      </div>
+
+      <div className="grid grid-cols-4 gap-4 mt-4">
+        <div>
+          {completedData?.map((item) => (
+            <DataCard key={item?.id} data={item}>
+              <div className="space-y-2 mt-2">
+                <p className="text-secondary text-xs font-medium leading-[21px] tracking-[-0.12px]">
+                  Attendees:{" "}
+                  <span className="text-light">{item.attendees}</span>
+                </p>
+                <p className="text-secondary text-xs font-medium leading-[21px] tracking-[-0.12px]">
+                  Last Activity:{" "}
+                  <span className="text-light">{item.status}</span>
+                </p>
+              </div>
+
+              <div className="mt-9 flex items-center justify-between gap-2">
+                <p className="flex items-center gap-1 text-[#ccc] text-xs font-medium leading-[18px] tracking-[-0.12px] py-1 px-2 rounded-full bg-secondPrimary">
+                  <span className="flex items-center gap-1">
+                    <CalenderSvg />
+                    {item?.date}
+                  </span>
+                  •<span className="flex items-center gap-1">{item?.time}</span>
+                </p>
+
+                <p className="flex items-center gap-1 text-[#ccc] text-xs font-medium leading-[18px] tracking-[-0.12px] py-1 px-2 rounded-full bg-secondPrimary">
+                  <span className="flex items-center gap-1">
+                    <ClockSvg />
+                    {item?.duration}
+                  </span>
+                </p>
+              </div>
+            </DataCard>
+          ))}
+        </div>
+        <div>
+          {scheduledData?.map((item) => (
+            <DataCard key={item?.id} data={item}>
+              <div className="space-y-2 mt-2">
+                <p className="text-secondary text-xs font-medium leading-[21px] tracking-[-0.12px]">
+                  Invited: <span className="text-light">{item.invited}</span>
+                </p>
+                <p className="text-secondary text-xs font-medium leading-[21px] tracking-[-0.12px]">
+                  Last Activity:{" "}
+                  <span className="text-light">{item.status}</span>
+                </p>
+              </div>
+
+              <div className="mt-9 flex items-center justify-between gap-2">
+                <p className="flex items-center gap-1 text-[#ccc] text-xs font-medium leading-[18px] tracking-[-0.12px] py-1 px-2 rounded-full bg-secondPrimary">
+                  <span className="flex items-center gap-1">
+                    <CalenderSvg />
+                    {item?.date}
+                  </span>
+                  •<span className="flex items-center gap-1">{item?.time}</span>
+                </p>
+              </div>
+            </DataCard>
+          ))}
+        </div>
+      </div>
+
+      <Pagination
+        currentPage={currentPage}
+        totalItems={45}
+        itemsPerPage={12}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
     </div>
   );
 };
