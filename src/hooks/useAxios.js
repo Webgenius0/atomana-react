37197af3bf900bb8301e06159e18ec421ad750b8
shanin18 <shamimhosan02@gmail.com>
2024-12-12 10:25:56 +0600

    added error respons

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,6 +1,40 @@
 import axiosConfig from "@/lib/axios.config";
+import { useCallback, useEffect } from "react";
 
 export function useAxios() {
-  const axios = axiosConfig;
+  return axiosConfig;
+}
+
+export function useAxiosSecure() {
+  const axios = useCallback(axiosConfig, []);
+
+  useEffect(() => {
+    axios.interceptors.request.use(
+      (config) => {
+        // Modify the request (e.g., add auth token)
+
+        return config;
+      },
+      (error) => {
+        console.log(error);
+        return error;
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    axios.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (err) => {
+        const status = err.response.status;
+        if (status === 401 || status === 403) {
+            
+        }
+      }
+    );
+  }, []);
+
   return axios;
 }
