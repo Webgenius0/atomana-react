9e55ac7fd0a61372aae4fc0638de815c55cf1e8c
tofayel <tofayeltuhin143@gmail.com>
2024-12-16 14:53:53 +0600

    merge with main

---
+++ src/components/svgs/ArrowRightSvg.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const ArrowRightSvg = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <g clip-path="url(#clip0_302_11174)">
+        <path
+          d="M6.53171 12.1886V10.8687L15.5827 10.8687L11.5286 6.8146L12.4714 5.8718L18.1283 11.5286L12.4714 17.1855L11.5286 16.2427L15.5827 12.1886H6.53171Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_302_11174">
+          <rect
+            width="16"
+            height="16"
+            fill="white"
+            transform="translate(12 0.686279) rotate(45)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default ArrowRightSvg;
---
+++ src/components/svgs/CrossSvg.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const CrossSvg = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <g clip-path="url(#clip0_255_1424)">
+        <path
+          d="M12.6668 4.27301L11.7268 3.33301L8.00016 7.05967L4.2735 3.33301L3.3335 4.27301L7.06016 7.99967L3.3335 11.7263L4.2735 12.6663L8.00016 8.93967L11.7268 12.6663L12.6668 11.7263L8.94016 7.99967L12.6668 4.27301Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_255_1424">
+          <rect width="16" height="16" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default CrossSvg;
