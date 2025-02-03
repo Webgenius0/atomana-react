b8d817491aa6b2860578d727f49975724d1661d0
Md. Tofayel Islam <tofayeltuhin143@gmail.com>
2025-02-03 17:10:11 +0600

    add video control

--- src/pages/VideoCourseDetails.jsx
+++ src/pages/VideoCourseDetails.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import ReactPlayer from "react-player";
 import {
-  FaArrowLeft,
+  FaArrowLeft, 
   FaArrowRight,
   FaCaretDown
 } from "react-icons/fa";
+import { RiArrowLeftDoubleLine } from "react-icons/ri";
+import {RiArrowRightDoubleLine } from "react-icons/ri";
+
 import { Link, useLocation } from "react-router-dom";
 
 const VideoCourseDetails = () => {
@@ -14,7 +17,6 @@ const VideoCourseDetails = () => {
   const [showMore, setShowMore] = useState(false);
   const toggleShowMore = () => setShowMore((prev) => !prev);
 
-  // Real YouTube video with timestamps
   const lessons = [
     {
       id: 1,
@@ -103,10 +105,6 @@ const VideoCourseDetails = () => {
       setCurrentLessonIndex(currentLessonIndex - 1);
     }
   };
-
-
-
-
   const courseDescription = `
   Learn how to create a powerful social media strategy tailored for real estate agents. 
   This course will cover platform selection, content creation, and leveraging social media 
@@ -115,110 +113,98 @@ const VideoCourseDetails = () => {
   and leveraging social media to attract and engage potential buyers.
 `;
   return (
-    <>
-      <div className="my-container">
-        <Link to={`${location.state?.from || "/my-classroom/courses"}`}>
-          <div className="flex items-center gap-3 mt-5 text-white">
-            <FaArrowLeft />
-            <h1 className="text-white text-lg font-medium">
-              {lessons[currentLessonIndex].title}
-            </h1>
-          </div>
-        </Link>
+    <div className="my-container">
+      <Link to={`${location.state?.from || "/my-classroom/courses"}`}>
+        <div className="flex items-center gap-3 mt-5 text-white">
+          <FaArrowLeft />
+          <h1 className="text-white text-lg font-medium">
+            {lessons[currentLessonIndex].title}
+          </h1>
+        </div>
+      </Link>
 
-        <div className="relative rounded-2xl bg-transparent mt-5">
-          <div className="rounded-xl overflow-hidden relative">
-            <ReactPlayer
-              url={lessons[currentLessonIndex].url}
-              controls
-              playing={true}
-              width="100%"
-              height="550px"
-              className="rounded-xl"
-            />
+      <div className="relative rounded-2xl bg-transparent mt-5">
+        <div className="rounded-xl overflow-hidden relative">
+          <ReactPlayer
+            url={lessons[currentLessonIndex].url}
+            controls
+            playing={true}
+            width="100%"
+            height="550px"
+            className="rounded-xl"
+          />
 
-            {/* Next & Previous Buttons */}
+          
+          <div className="absolute bottom-16 left-1/2 transform -translate-x-1/2 flex gap-5">
             <button
               onClick={goToPreviousVideo}
               disabled={currentLessonIndex === 0}
-              className={`absolute left-2 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-60 p-3 rounded-full text-white ${
+              className={`bg-black bg-opacity-50 p-3 rounded-full text-white transition ${
                 currentLessonIndex === 0 ? "opacity-50 cursor-not-allowed" : "hover:bg-opacity-80"
               }`}
             >
-              <FaArrowLeft size={20} />
+              <RiArrowLeftDoubleLine size={22} />
             </button>
 
             <button
               onClick={goToNextVideo}
               disabled={currentLessonIndex === lessons.length - 1}
-              className={`absolute right-2 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-60 p-3 rounded-full text-white ${
+              className={`bg-black bg-opacity-50 p-3 rounded-full text-white transition ${
                 currentLessonIndex === lessons.length - 1 ? "opacity-50 cursor-not-allowed" : "hover:bg-opacity-80"
               }`}
             >
-              <FaArrowRight size={20} />
+              <RiArrowRightDoubleLine size={22} />
             </button>
           </div>
+        </div>
 
-          <div className="mt-5">
-            <h1 className="mt-5 mb-10 text-light">
-              Lessons in this class
-            </h1>
+        <div className="mt-5">
+          <h1 className="mt-5 mb-10 text-light">Lessons in this class</h1>
+          <div className="mb-6">
+            {lessons
+              .slice(0, showAllLessons ? lessons.length : 5)
+              .map((lesson, index) => (
+                <div
+                  key={lesson.id}
+                  className={`flex items-center justify-between gap-4 mb-4 border-b ${
+                    index === currentLessonIndex ? "text-[#009696] cursor-pointer" : "text-light"
+                  }`}
+                  onClick={() => setCurrentLessonIndex(index)}
+                >
+                  <h3 className="text-sm font-bold pb-2.5">{lesson.title}</h3>
+                  <p>{lesson.duration}</p>
+                </div>
+              ))}
 
-            <div className="mb-6">
-              {lessons
-                .slice(0, showAllLessons ? lessons.length : 5)
-                .map((lesson, index) => (
-                  <div
-                    key={lesson.id}
-                    className={`flex items-center justify-between gap-4 mb-4 border-b ${
-                      index === currentLessonIndex ? "text-[#009696] cursor-pointer" : "text-light"
-                    }`}
-                    onClick={() => setCurrentLessonIndex(index)}
-                  >
-                    <h3 className="text-sm font-bold pb-2.5">
-                      {lesson.title}
-                    </h3>
-                    <p>{lesson.duration}</p>
-                  </div>
-                ))}
+            <p
+              onClick={toggleLessons}
+              className="flex justify-center items-center text-[#009696] gap-1 cursor-pointer"
+            >
+              {showAllLessons ? "SHOW LESS" : "SHOW ALL LESSONS"}
+              <FaCaretDown className={`transform transition-transform ${showAllLessons ? "rotate-180" : ""}`} />
+            </p>
 
-              <p
-                onClick={toggleLessons}
-                className="flex justify-center items-center text-[#009696] gap-1 cursor-pointer"
-              >
-                {showAllLessons ? "SHOW LESS" : "SHOW ALL LESSONS"}
-                <FaCaretDown className={`transform transition-transform ${showAllLessons ? "rotate-180" : ""}`} />
-              </p>
-
-
-
-
-              <div className="text-light">
-                <h1 className="section-title mt-3 sm:mt-4 md:mt-5 mb-3 sm:mb-4 md:mb-5">
-                  About This Course
-                </h1>
-                <p className="text-light text-sm leading-5">
+            <div className="text-light">
+              <h1 className="section-title mt-3 sm:mt-4 md:mt-5 mb-3 sm:mb-4 md:mb-5">
+                About This Course
+              </h1>
+              <p className="text-light text-sm leading-5">
                   {showMore
                     ? courseDescription
                     : `${courseDescription.slice(0, 400)}...`}
                 </p>
-              </div>
-              <p
-                onClick={toggleShowMore}
-                className="flex justify-center items-center text-[#009696] gap-1 mt-3 sm:mt-4 md:mt-5 cursor-pointer"
-              >
-                {showMore ? "SHOW LESS" : "SHOW MORE"}
-                <FaCaretDown
-                  className={`transform transition-transform ${
-                    showMore ? "rotate-180" : ""
-                  }`}
-                />
-              </p>
             </div>
+            <p
+              onClick={toggleShowMore}
+              className="flex justify-center items-center text-[#009696] gap-1 mt-3 sm:mt-4 md:mt-5 cursor-pointer"
+            >
+              {showMore ? "SHOW LESS" : "SHOW MORE"}
+              <FaCaretDown className={`transform transition-transform ${showMore ? "rotate-180" : ""}`} />
+            </p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
