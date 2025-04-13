e2a5a1be7e78a1c9b4c4f31061d279236ffa514b
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2025-04-13 15:47:07 +0600

    update custom date picker

--- src/components/CustomDatePicker.jsx
+++ src/components/CustomDatePicker.jsx
@@ -4,26 +4,18 @@ import ReactDatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const CustomDatePicker = ({ value, onChange, className }) => {
-  // const [startDate, setStartDate] = useState(new Date());
   const [open, setOpen] = useState(false);
 
   return (
-    //   <ReactDatePicker
-    //   selected={startDate}
-    //   onChange={(date) => setStartDate(date)}
-    //   className="bg-inherit focus:outline-none py-3 w-full placeholder:text-secondary text-light text-sm leading-[21px] tracking-[-0.14px]"
-    //   placeholderText="DD/MM/YYYY"
-    //   dateFormat="dd/MM/yyyy"
-    // />
-
     <ReactDatePicker
       selected={value}
       onChange={(date) => {
         onChange(date);
-        setOpen(false); // Close calendar after selecting a date
+        setOpen(false);
       }}
-      onFocus={() => setOpen(true)} // Open when focusing on the input
-      open={open} // Control calendar state
+      onFocus={() => setOpen(true)}
+      onBlur={() => setOpen(false)}
+      open={open}
       className={cn(
         'bg-inherit focus:outline-none py-3 w-full placeholder:text-secondary text-light text-sm leading-[21px] tracking-[-0.14px]',
         className
