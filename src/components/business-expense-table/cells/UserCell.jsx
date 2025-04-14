4ebeeed0ed94b9e62d3dc2ec69c5e5eaf94c449f
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2025-04-14 13:27:00 +0600

    implement owner cell edit on my business table

--- src/components/business-expense-table/cells/UserCell.jsx
+++ src/components/business-expense-table/cells/UserCell.jsx
@@ -1,37 +1,31 @@
 import { useDataTable } from '@/components/table/DataTableContext';
+import { useGetAgents } from '@/hooks/expense.hook';
+
 import { Button } from '@/components/ui/button';
 import { Select } from '@/components/ui/select';
 import { useUpdateBusinessExpense } from '@/hooks/expense.hook';
-import { useGetPaymentMethods } from '@/hooks/payment.hook';
 import { useQueryClient } from '@tanstack/react-query';
 import { CheckIcon, PencilIcon, XIcon } from 'lucide-react';
 import { Controller, useForm } from 'react-hook-form';
 
-export default function PaymentMethodCell({ getValue, row, column }) {
+export default function UserCell({ getValue, row, column }) {
   const rowId = row?.original?.id;
   const columnId = column?.id;
   const value = getValue();
 
   const [editableCell, setEditableCell] = useDataTable();
-  const { paymentMethods, isLoading } = useGetPaymentMethods();
+  const { agents, isLoading } = useGetAgents();
   const queryClient = useQueryClient();
-  const { mutate, isPending } = useUpdateBusinessExpense(
-    rowId,
-    'payment-method'
-  );
+  const { mutate, isPending } = useUpdateBusinessExpense(rowId, 'user');
   const form = useForm({
     defaultValues: {
       [columnId]: value,
     },
   });
 
-  const displayedValue = paymentMethods?.find(
-    (method) => method.id === value
-  )?.name;
-
-  const categoryOptions = paymentMethods?.map((item) => ({
-    value: item.name,
-    label: item.name,
+  const agentOptions = agents?.map((item) => ({
+    value: `${item.first_name} ${item.last_name}`,
+    label: `${item.first_name} ${item.last_name}`,
   }));
 
   const onSubmit = (data) => {
@@ -50,23 +44,31 @@ export default function PaymentMethodCell({ getValue, row, column }) {
     return (
       <div className="flex items-center gap-2 relative">
         <Controller
-          name="payment_method_id"
+          name="user_id"
           control={form.control}
-          render={({ field }) => (
-            <Select
-              value={
-                paymentMethods?.find((item) => item.id === field.value)?.name
-              }
-              setValue={(value) =>
-                field.onChange(
-                  paymentMethods?.find((item) => item.name === value).id
-                )
-              }
-              disabled={isLoading}
-              options={categoryOptions}
-              placeholder="Select Payment Method"
-            />
-          )}
+          render={({ field }) => {
+            const currentValue = agents?.find(
+              (item) => item.id === field.value
+            );
+            return (
+              <Select
+                value={
+                  currentValue &&
+                  `${currentValue?.first_name} ${currentValue?.last_name}`
+                }
+                setValue={(value) =>
+                  field.onChange(
+                    agents?.find(
+                      (item) => `${item.first_name} ${item.last_name}` === value
+                    ).id
+                  )
+                }
+                disabled={isLoading}
+                options={agentOptions}
+                placeholder="Select Owner"
+              />
+            );
+          }}
         />
 
         <div className="flex items-center absolute bottom-full right-0">
@@ -94,13 +96,15 @@ export default function PaymentMethodCell({ getValue, row, column }) {
   }
 
   // View Only Cell
+
   return (
     <div
-      className="px-[10px] py-[6.5px] relative group"
+      className="px-[10px] py-[6.5px] first-letter:uppercase relative group"
       onClick={() => setEditableCell(null)}
       onDoubleClick={() => setEditableCell(`${columnId}-${rowId}`)}
     >
-      {displayedValue || '-'}
+      {value || '-'}
+
       <button
         className="hidden group-hover:flex absolute top-1/2 right-2 -translate-y-1/2 text-white/60 hover:text-white"
         type="button"
