0b9c3ed547e7cfc4ada9c4deb6c582fc7130cfb5
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2025-04-14 17:46:00 +0600

    add view listing information dummy page

---
+++ src/pages/listing-information/ViewListingInformation.jsx
@@ -0,0 +1,186 @@
+import AccessInstructionSkeleton from '@/components/access-instruction/AccessInstructionSkeleton';
+import ArrowLeftSvg from '@/components/svgs/ArrowLeftSvg';
+import ThreeDotsSvg from '@/components/svgs/ThreeDotsSvg';
+import { useGetSingleAccessInstruction } from '@/hooks/access-instructions.hook';
+import { formatCurrency } from '@/lib/utils/formatCurrency';
+import DOMPurify from 'dompurify';
+import { Link, useParams } from 'react-router-dom';
+
+const ViewListingInformation = () => {
+  const { id } = useParams();
+
+  const { accessInstruction, isLoading } = useGetSingleAccessInstruction(id);
+
+  const notes = accessInstruction?.notes
+    ? DOMPurify.sanitize(accessInstruction?.notes)
+    : '';
+
+  if (isLoading) {
+    return <AccessInstructionSkeleton />;
+  }
+
+  return (
+    <>
+      <div className="flex items-center gap-4 justify-between">
+        <div>
+          <Link
+            to="/my-systems/team/access-instructions"
+            className="flex items-center gap-5 duration-300 hover:opacity-60 w-fit my-5"
+          >
+            <ArrowLeftSvg />
+          </Link>
+          <div>
+            <h2 className="section-title">Listing Information</h2>
+            <p className="text-sm text-[#009696] leading-[21px] tracking-[-0.14px]">
+              {accessInstruction?.address || 'N/A'}
+            </p>
+          </div>
+        </div>
+        <button className="w-10 h-10 rounded-full border border-secondPrimary flex items-center justify-center duration-300 active:scale-95">
+          <ThreeDotsSvg />
+        </button>
+      </div>
+
+      <div className="mt-5">
+        <h2 className="section-title">Property Details</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-12 md:mt-2">
+          {/* Address */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
+            <p className="font-bold leading-5 text-sm text-[#ffffffcc]">
+              Address
+            </p>
+            <p className="text-sm font-normal leading-5 text-[#009696]">
+              {accessInstruction?.address || 'N/A'}
+            </p>
+          </div>
+
+          {/* Property Type */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
+            <p className="font-bold leading-5 text-sm text-[#ffffffcc]">
+              Property Type
+            </p>
+            <p className="text-sm font-normal leading-5 text-[#009696]">
+              {accessInstruction?.property_type || 'N/A'}
+            </p>
+          </div>
+
+          {/* Price */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
+            <p className="font-bold leading-5 text-sm text-[#ffffffcc]">
+              Price
+            </p>
+            <p className="text-sm font-normal leading-5 text-[#009696]">
+              {accessInstruction?.price
+                ? formatCurrency(accessInstruction?.price)
+                : 'N/A'}
+            </p>
+          </div>
+
+          {/* Size */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
+            <p className="font-bold leading-5 text-sm text-[#ffffffcc]">Size</p>
+            <p className="text-sm font-normal leading-5 text-[#009696]">
+              {accessInstruction?.size
+                ? `${accessInstruction?.size} sq ft`
+                : 'N/A'}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-[25px]">
+        <h2 className="section-title">Access Instructions</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2 md:gap-12 md:mt-4">
+          {/* Key Access Code */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
+            <p className="font-bold leading-5 text-sm text-[#ffffffcc]">
+              Key Access Code
+            </p>
+            <p className="text-sm font-normal leading-5 text-[#ffffffcc]">
+              {accessInstruction?.key_access_code || 'N/A'}
+            </p>
+          </div>
+
+          {/* Lockbox Location */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
+            <p className="font-bold leading-5 text-sm text-[#ffffffcc]">
+              Lockbox Location
+            </p>
+            <p className="text-sm font-normal leading-5 text-[#ffffffcc]">
+              {accessInstruction?.lockbox_location || 'N/A'}
+            </p>
+          </div>
+
+          {/* Key Pickup Instructions */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
+            <p className="font-bold leading-5 text-sm text-[#ffffffcc]">
+              Key Pickup Instructions (if applicable):
+            </p>
+            <p className="text-sm font-normal leading-5 text-[#ffffffcc]">
+              {accessInstruction?.key_pickup_instructions || 'N/A'}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-[25px]">
+        <h2 className="section-title">Gated Community Instructions</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2 md:gap-12 md:mt-4">
+          {/* Gate Code */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
+            <p className="font-bold leading-5 text-sm text-[#ffffffcc]">
+              Gate Code
+            </p>
+            <p className="text-sm font-normal leading-5 text-[#ffffffcc]">
+              {accessInstruction?.gate_code || 'N/A'}
+            </p>
+          </div>
+
+          {/* Gate Access Location */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
+            <p className="font-bold leading-5 text-sm text-[#ffffffcc]">
+              Gate Access Location
+            </p>
+            <p className="text-sm font-normal leading-5 text-[#ffffffcc]">
+              {accessInstruction?.gate_access_location || 'N/A'}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-[25px]">
+        <h2 className="section-title">Parking</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2 md:gap-12 md:mt-4">
+          {/* Visitor Parking */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
+            <p className="font-bold leading-5 text-sm text-[#ffffffcc]">
+              Visitor Parking
+            </p>
+            <p className="text-sm font-normal leading-5 text-[#ffffffcc]">
+              {accessInstruction?.visitor_parking || 'N/A'}
+            </p>
+          </div>
+        </div>
+      </div>
+      {notes && (
+        <div className="mt-[25px]">
+          <h2 className="section-title">Additional Notes</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2 md:gap-4 md:mt-4">
+            {/* Notes */}
+            <div className="space-y-[2px]  py-4">
+              <p className="font-bold leading-5 text-sm text-[#ffffffcc]">
+                Notes
+              </p>
+
+              <div
+                className="rich-text"
+                dangerouslySetInnerHTML={{ __html: notes }}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ViewListingInformation;
