5b1a9f9803c8d8c0335a736e29f66253b88417b5
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2025-04-14 12:52:12 +0600

    add edit icon

--- src/pages/docs/ViewSinglePassword.jsx
+++ src/pages/docs/ViewSinglePassword.jsx
@@ -4,6 +4,7 @@ import ThreeDotsSvg from '@/components/svgs/ThreeDotsSvg';
 import { useGetSinglePassword } from '@/hooks/docs.hook';
 import { format } from 'date-fns';
 import DOMPurify from 'dompurify';
+import { EditIcon } from 'lucide-react';
 import { useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
@@ -51,6 +52,9 @@ export default function ViewSinglePassword() {
         </Link>
         <h2 className="section-title">{passwordDetails?.title}</h2>
         <div className="flex items-center gap-2.5 ml-auto">
+          <button className="w-10 h-10 rounded-full border border-secondPrimary flex items-center justify-center duration-300 active:scale-95">
+            <EditIcon className="text-white size-4" />
+          </button>
           <button className="w-10 h-10 rounded-full border border-secondPrimary flex items-center justify-center duration-300 active:scale-95">
             <PersonPlusSvg />
           </button>
