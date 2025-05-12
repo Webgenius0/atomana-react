45c55c90181c5ff632cacaf4b6064142d15047ba
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2025-05-12 18:39:33 +0600

    integrate delete listing information api

--- src/hooks/new-listing-information.js
+++ src/hooks/new-listing-information.js
@@ -1,4 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
 import { useAxiosSecure } from './useAxios';
 
 export const useGetListingInformation = ({ perPage = 10, currentPage = 1 }) => {
@@ -31,4 +33,35 @@ export const useGetListingInformation = ({ perPage = 10, currentPage = 1 }) => {
     totalItems,
     per_page,
   };
+};
+
+export const useDeleteListingInformation = () => {
+  const [rowSelection, setRowSelection] = useState({});
+  const axiosPrivate = useAxiosSecure();
+  const queryClient = useQueryClient();
+
+  const result = useMutation({
+    mutationFn: async (ids) => {
+      const response = await axiosPrivate.delete(`/api/v1/property/`, {
+        data: {
+          id: ids,
+        },
+      });
+      return response.data;
+    },
+    onSuccess: (data) => {
+      if (data?.success) {
+        queryClient.invalidateQueries(['listing-information']);
+        toast.success(
+          data?.message || 'Listing Information deleted successfully'
+        );
+        setRowSelection({});
+      }
+    },
+    onError: (error) => {
+      toast.error(error?.response?.data?.message);
+    },
+  });
+
+  return { ...result, rowSelection, setRowSelection };
 };
