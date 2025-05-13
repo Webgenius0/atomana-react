faef07fa2881f38ea3ae0ab4f35670c9ee07ff87
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2025-05-13 21:35:39 +0600

    allow host

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +8,10 @@ export default defineConfig({
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  preview: {
+    allowedHosts: ['app.myopsre.com'],
   },
 });
