0f3e88e200d667b5839bcab8532e3a2f231c82a3
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2025-05-18 09:42:22 +0600

    refactor data view table

--- src/components/open-house-list/cells/AddressCell.jsx
+++ src/components/open-house-list/cells/AddressCell.jsx
@@ -1,12 +1,18 @@
-export default function AddressCell({ getValue }) {
+import { useNavigate } from 'react-router-dom';
+
+export default function AddressCell({ getValue, row }) {
   const value = getValue();
+  const navigate = useNavigate();
+
   return (
     <div
       title={value}
-      className="px-[10px] py-[6.5px] first-letter:uppercase"
-      //   style={{
-      //     width: `${cell.column.getSize()}px`,
-      //   }}
+      className="px-[10px] py-[6.5px] first-letter:uppercase cursor-pointer hover:text-light/60"
+      onClick={() => {
+        if (row?.original?.path) {
+          navigate(row?.original?.path);
+        }
+      }}
     >
       {value || '-'}
     </div>
