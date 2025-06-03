625a4b6e1ec88a6047ebc2dfb2fea62831af5c04
jakareya <jakareya1306@gmail.com>
2025-06-03 15:18:11 +0600

    view listing information modified

--- src/pages/listing-information/ViewListingInformation.jsx
+++ src/pages/listing-information/ViewListingInformation.jsx
@@ -1,11 +1,11 @@
-import AccessInstructionSkeleton from '@/components/access-instruction/AccessInstructionSkeleton';
-import ArrowLeftSvg from '@/components/svgs/ArrowLeftSvg';
-import ThreeDotsSvg from '@/components/svgs/ThreeDotsSvg';
-import { useGetSingleListingInformation } from '@/hooks/new-listing-information';
+import AccessInstructionSkeleton from "@/components/access-instruction/AccessInstructionSkeleton";
+import ArrowLeftSvg from "@/components/svgs/ArrowLeftSvg";
+import ThreeDotsSvg from "@/components/svgs/ThreeDotsSvg";
+import { useGetSingleListingInformation } from "@/hooks/new-listing-information";
 
-import { formatCurrency } from '@/lib/utils/formatCurrency';
-import DOMPurify from 'dompurify';
-import { Link, useParams } from 'react-router-dom';
+import { formatCurrency } from "@/lib/utils/formatCurrency";
+import DOMPurify from "dompurify";
+import { Link, useParams } from "react-router-dom";
 
 const ViewListingInformation = () => {
   const { id } = useParams();
@@ -28,7 +28,7 @@ const ViewListingInformation = () => {
           <div>
             <h2 className="section-title">Listing Information</h2>
             <p className="text-sm text-[#009696] leading-[21px] tracking-[-0.14px]">
-              {listingInformation?.address || 'N/A'}
+              {listingInformation?.address || "N/A"}
             </p>
           </div>
         </div>
@@ -37,59 +37,39 @@ const ViewListingInformation = () => {
         </button>
       </div>
 
+      {/* Property Details */}
       <div className="mt-5">
         <h2 className="section-title">Property Details</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-12 md:mt-2">
-          {/* <div className="space-y-[2px] border-b border-secondPrimary py-4">
-            <p className="font-bold text-sm text-[#ffffffcc]">SKU</p>
-            <p className="text-sm text-[#009696]">
-              {listingInformation?.sku || 'N/A'}
-            </p>
-          </div> */}
-
-          {/* <div className="space-y-[2px] border-b border-secondPrimary py-4">
-            <p className="font-bold text-sm text-[#ffffffcc]">Email</p>
-            <p className="text-sm text-[#009696]">
-              {listingInformation?.email || 'N/A'}
-            </p>
-          </div> */}
-
+          {/* Address field */}
           <div className="space-y-[2px] border-b border-secondPrimary py-4">
             <p className="font-bold text-sm text-[#ffffffcc]">Address</p>
             <p className="text-sm text-[#009696]">
-              {listingInformation?.address || 'N/A'}
+              {listingInformation?.address || "N/A"}
             </p>
           </div>
 
+          {/* Price field */}
           <div className="space-y-[2px] border-b border-secondPrimary py-4">
             <p className="font-bold text-sm text-[#ffffffcc]">Price</p>
             <p className="text-sm text-[#009696]">
               {listingInformation?.price
                 ? formatCurrency(listingInformation.price)
-                : 'N/A'}
+                : "N/A"}
             </p>
           </div>
 
+          {/* Expiration Date field */}
           <div className="space-y-[2px] border-b border-secondPrimary py-4">
             <p className="font-bold text-sm text-[#ffffffcc]">
               Expiration Date
             </p>
             <p className="text-sm text-[#009696]">
-              {listingInformation?.expiration_date || 'N/A'}
+              {listingInformation?.expiration_date || "N/A"}
             </p>
           </div>
-          {/* <p className="text-sm text-[#009696]">
-  {accessInstruction?.commission_rate
-    ? `${accessInstruction.commission_rate}%`
-    : "N/A"}
-</p>
 
-<p className="text-sm text-[#009696]">
-  {accessInstruction?.co_list_percentage
-    ? `${accessInstruction.co_list_percentage}%`
-    : "N/A"}
-</p> */}
-
+          {/* Commission Rate field */}
           <div className="space-y-[2px] border-b border-secondPrimary py-4">
             <p className="font-bold text-sm text-[#ffffffcc]">
               Commission Rate
@@ -97,10 +77,23 @@ const ViewListingInformation = () => {
             <p className="text-sm text-[#009696]">
               {listingInformation?.commission_rate
                 ? `${listingInformation.commission_rate}%`
-                : 'N/A'}
+                : "N/A"}
             </p>
           </div>
 
+          {/* Commission Split field */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
+            <p className="font-bold text-sm text-[#ffffffcc]">
+              Commission Split
+            </p>
+            <p className="text-sm text-[#009696]">
+              {listingInformation?.commission_split
+                ? `${listingInformation.commission_split}`
+                : "N/A"}
+            </p>
+          </div>
+
+          {/* Co List Percentage field */}
           <div className="space-y-[2px] border-b border-secondPrimary py-4">
             <p className="font-bold text-sm text-[#ffffffcc]">
               Co List Percentage
@@ -108,76 +101,58 @@ const ViewListingInformation = () => {
             <p className="text-sm text-[#009696]">
               {listingInformation?.co_list_percentage
                 ? `${listingInformation.co_list_percentage}%`
-                : 'N/A'}
+                : "N/A"}
             </p>
           </div>
 
-          {/* <div className="space-y-[2px] border-b border-secondPrimary py-4">
-            <p className="font-bold text-sm text-[#ffffffcc]">
-              Property Source ID
-            </p>
-            <p className="text-sm text-[#009696]">
-              {listingInformation?.property_source_id || 'N/A'}
-            </p>
-          </div> */}
-
-          <div className="space-y-[2px] border-b border-secondPrimary py-4">
-            <p className="font-bold text-sm text-[#ffffffcc]">
-              Is Development?
-            </p>
-            <p className="text-sm text-[#009696]">
-              {listingInformation?.is_development ? 'Yes' : 'No'}
-            </p>
-          </div>
-
-          <div className="space-y-[2px] border-b border-secondPrimary py-4">
-            <p className="font-bold text-sm text-[#ffffffcc]">Add to Website</p>
-            <p className="text-sm text-[#009696]">
-              {listingInformation?.add_to_website ? 'Yes' : 'No'}
-            </p>
-          </div>
-
-          <div className="space-y-[2px] border-b border-secondPrimary py-4 col-span-full">
+          {/* Agent field */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4 col-span-full lg:col-span-1">
             <p className="font-bold text-sm text-[#ffffffcc]">Agent</p>
             <p className="text-sm text-[#009696]">
               {listingInformation?.agent
                 ? `${listingInformation.agent.first_name} ${listingInformation.agent.last_name}`
-                : 'N/A'}
+                : "N/A"}
             </p>
           </div>
+
+          {/* Beds field */}
           <div className="space-y-[2px] border-b border-secondPrimary py-4">
             <p className="font-bold text-sm text-[#ffffffcc]">Beds</p>
             <p className="text-sm text-[#009696]">
-              {listingInformation?.beds ?? 'N/A'}
+              {listingInformation?.beds ?? "N/A"}
             </p>
           </div>
 
+          {/* Full Baths field */}
           <div className="space-y-[2px] border-b border-secondPrimary py-4">
             <p className="font-bold text-sm text-[#ffffffcc]">Full Baths</p>
             <p className="text-sm text-[#009696]">
-              {listingInformation?.full_baths ?? 'N/A'}
+              {listingInformation?.full_baths ?? "N/A"}
             </p>
           </div>
 
+          {/* Half Baths field */}
           <div className="space-y-[2px] border-b border-secondPrimary py-4">
             <p className="font-bold text-sm text-[#ffffffcc]">Half Baths</p>
             <p className="text-sm text-[#009696]">
-              {listingInformation?.half_baths ?? 'N/A'}
+              {listingInformation?.half_baths ?? "N/A"}
             </p>
           </div>
 
+          {/* Size field */}
           <div className="space-y-[2px] border-b border-secondPrimary py-4">
             <p className="font-bold text-sm text-[#ffffffcc]">Size</p>
             <p className="text-sm text-[#009696]">
               {listingInformation?.size
                 ? `${Number(listingInformation?.size)
                     ?.toFixed(0)
-                    .padStart(2, '0')} SQFT`
-                : 'N/A'}
+                    .padStart(2, "0")} SQFT`
+                : "N/A"}
             </p>
           </div>
 
-          <div className="space-y-[2px] border-b border-secondPrimary py-4 col-span-full">
+          {/* External Link field */}
+          <div className="space-y-[2px] border-b border-secondPrimary py-4">
             <p className="font-bold text-sm text-[#ffffffcc]">External Link</p>
             <p className="text-sm text-[#009696] break-all">
               {listingInformation?.link ? (
@@ -190,13 +165,14 @@ const ViewListingInformation = () => {
                   {listingInformation.link}
                 </a>
               ) : (
-                'N/A'
+                "N/A"
               )}
             </p>
           </div>
         </div>
       </div>
 
+      {/* Additional Notes */}
       {listingInformation?.note && (
         <div className="mt-6">
           <h2 className="section-title">Additional Notes</h2>
